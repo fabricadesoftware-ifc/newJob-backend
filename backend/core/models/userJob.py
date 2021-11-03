589189860f876fccbf6fe6c80497e0f511e2692a
Lucas <lucas.beckhauser@projuris.com>
2021-11-03 12:41:52 -0300

    Criando model userJob

---
+++ backend/core/models/userJob.py
@@ -0,0 +1,11 @@
+from django.db import models
+from .state import State
+from .user import User
+
+
+class UserJob(models.Model):
+    user = models.ForeignKey(State, on_delete=models.CASCADE)
+    job = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.job
