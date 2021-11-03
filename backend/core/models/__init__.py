b8fbc19687a2f53a81a27260fee01c954e2d31e4
Lucas <lucas.beckhauser@projuris.com>
2021-11-03 12:43:34 -0300

    Adicionando novos models ao __init__

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,3 +1,7 @@
 from .user import User
 from .company import Company
 from .contractType import ContractType
+from .job import Job
+from .local import Local
+from .state import State
+from .userJob import UserJob
