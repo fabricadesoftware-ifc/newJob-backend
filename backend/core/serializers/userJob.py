c82bd7499850ed17fd2f350026bd2f1a2d65f89c
Lucas <lucas.beckhauser@projuris.com>
2021-12-22 19:42:52 -0300

    Criando UserJobSerializer

---
+++ backend/core/serializers/userJob.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from backend.core.models import UserJob
+
+
+class UserJobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserJob
+        fields = ["id", "user", "job"]
