636545d389dd9c7c708c32c6d56d65dc7e3ca818
Lucas <lucas.beckhauser@projuris.com>
2021-12-22 20:17:53 -0300

    Atualizando init com novas ViewSet

--- backend/core/views/__init__.py
+++ backend/core/views/__init__.py
@@ -1,3 +1,7 @@
 from .auth import AuthView
 from .company import CompanyViewSet
 from .contractType import ContractTypeViewSet
+from .job import JobViewSet
+from .local import LocalViewSet
+from .state import StateViewSet
+from .userJob import UserJobViewSet
