8074e62edd0725c0bedc73c5aae96619c046b576
beckhauser <beckhauser125@gmail.com>
2022-02-01 14:08:35 -0300

    Puxando informações das chaves estrangeiras de job

--- backend/core/serializers/job.py
+++ backend/core/serializers/job.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from backend.core.models import Job
+from backend.core.models import Job, ContractType
+from backend.core.serializers.company import CompanyDetailSerializer
+from backend.core.serializers.contractType import ContractTypeSerializer
+from backend.core.serializers.local import LocalSerializer
 
 
 class JobSerializer(serializers.ModelSerializer):
+
+    contract_types = ContractTypeSerializer(read_only=True, many=True)
+    company = CompanyDetailSerializer(read_only=True)
+    local = LocalSerializer(read_only=True)
+ 
     class Meta:
         model = Job
         fields = ["id", "title", "description", "local", "contract_types", "company", "deadline"]
