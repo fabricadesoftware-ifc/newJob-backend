c6ab94453a63c00ba98dfbe0533e2f451e51a1d6
Beckhauser <beckhauser125@gmail.com>
2022-02-24 19:45:22 -0300

    Changing cood names to longitude and latitude

--- backend/core/serializers/local.py
+++ backend/core/serializers/local.py
@@ -5,4 +5,4 @@ from backend.core.models import Local
 class LocalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Local
-        fields = ["cood_x", "cood_y", "street_number", "street_name", "state"]
+        fields = ["longitude", "latitude", "street_number", "street_name", "state"]
