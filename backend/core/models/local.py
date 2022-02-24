8f794dd57c4d64df3c62b4a8ab0684adb34ccc48
Beckhauser <beckhauser125@gmail.com>
2022-02-24 19:45:35 -0300

    Adding city to Local

--- backend/core/models/local.py
+++ backend/core/models/local.py
@@ -3,11 +3,12 @@ from .state import State
 
 
 class Local(models.Model):
-    cood_x = models.CharField(max_length=255)
-    cood_y = models.CharField(max_length=255)
+    longitude = models.BigIntegerField()
+    latitude = models.BigIntegerField()
+    city = models.CharField(max_length=85)
     street_number = models.CharField(max_length=255)
     street_name = models.CharField(max_length=255)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.cood_x, self.cood_y
+        return self.longitude, self.latitude
