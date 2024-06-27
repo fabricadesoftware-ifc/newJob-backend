61232a3e055a5ed39af82b912fda207bc9b34b4c
GabrielLima2803 <gabriellima2803@gmail.com>
2024-06-27 15:31:51 -0300

    feat - Adicionando o diagrama do banco

--- backend/settings.py
+++ backend/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "django_extensions",
     "drf_spectacular",
     "rest_framework",
     "rest_registration",
