e8db3476c877d44eb06570e50989f64c3cd8ca67
GabrielLima2803 <gabriellima2803@gmail.com>
2024-08-14 13:30:54 -0300

    fix: Tabela errada na associação de job

--- backend/core/models/userJob.py
+++ backend/core/models/userJob.py
@@ -1,11 +1,11 @@
 from django.db import models
-from .state import State
+from .job import Job
 from .user import User
 
 
 class UserJob(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    job = models.ForeignKey(State, on_delete=models.CASCADE)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.job
