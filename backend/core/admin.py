ed075a4815e14306d2d0bef5d39521a05592caba
GabrielLima2803 <gabriellima2803@gmail.com>
2024-09-29 15:22:01 -0300

    feat: Adição das urls do enpoints e dos admins

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from backend.core.models import User, Company
+from backend.core.models import User, Company, Benefit, Category, ContractType, Job, Local, State
 
 admin.site.register(User)
 admin.site.register(Company)
+admin.site.register(Category)
+admin.site.register(ContractType)
+admin.site.register(Job)
+admin.site.register(Local)
+admin.site.register(State)
