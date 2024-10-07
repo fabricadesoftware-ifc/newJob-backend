366e0ef55c3f56b27da487a1789f2b7b918a6d52
GabrielLima2803 <gabriellima2803@gmail.com>
2024-10-07 16:16:01 -0300

    feat: Adicionando validações para a table Local

--- backend/core/serializers/local.py
+++ backend/core/serializers/local.py
@@ -6,7 +6,18 @@ from backend.core.serializers.state import StateSerializer
 class LocalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Local
-        fields = ["id","longitude", "latitude", "street_number", "street_name", "city", "state"]
+        fields = ["id", "latitude", "longitude", "street_number", "street_name", "city", "state"]
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        
+        try:
+            representation['longitude'] = float(instance.longitude) if instance.longitude is not None else None
+            representation['latitude'] = float(instance.latitude) if instance.latitude is not None else None
+        except InvalidOperation:
+            representation['longitude'] = "Valor inválido"
+            representation['latitude'] = "Valor inválido"
+        
+        return representation
 
 
 class LocalDetailSerializer(serializers.ModelSerializer):
