cd9616fc9269a95b94660bf0962f277b052f423d
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-12 16:32:10 -0300

    feat: Criação da view User

---
+++ backend/core/views/user.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from backend.core.models import User
+from backend.core.serializer import UserProfileSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserProfileSerializer
+
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        """Retorna o usuário autenticado"""
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
