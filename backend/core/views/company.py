04e3d73b5f13f6bd799bb74a0e741a6c17a74188
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-14 16:45:14 -0300

    FEAT: Criação de novos endpoints

--- backend/core/views/company.py
+++ backend/core/views/company.py
@@ -1,7 +1,12 @@
-from rest_framework import viewsets
-
+from rest_framework import viewsets, status
 from backend.core.models import Company
-from backend.core.serializers import CompanySerializer, CompanyDetailSerializer
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from backend.core.models import JobApplication, Job
+from backend.core.serializers import JobApplicationSerializer, CompanyDetailSerializer, CompanySerializer
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -17,3 +22,56 @@ class CompanyViewSet(viewsets.ModelViewSet):
 
     def get_serializer_class(self):
         return self.serializer_classes.get(self.action, self.default_serializer_class)
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def select_candidate(self, request, pk=None):
+        """
+        Permite que a empresa selecione um candidato para uma vaga e remove outros candidatos da vaga.
+        """
+        application = self.get_object()
+
+        job = application.job
+        if job.company != request.user.company:
+            return Response(
+                {"error": "Você não tem permissão para escolher um candidato para essa vaga."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        if application.is_selected:
+            return Response(
+                {"error": "Este candidato já foi selecionado."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        job.applications.filter(is_selected=True).update(is_selected=False, status=JobApplication.StatusJob.NÃO_SELECIONADO)
+
+        application.is_selected = True
+        application.status = JobApplication.StatusJob.SELECIONADO 
+        application.save()
+
+        job.selected_user = application.user
+        job.save()
+
+        subject = 'Candidato Selecionado'
+        from_email = 'gabriellima2803@gmail.com' 
+        to_email = [application.user.email]  
+
+        html_content = render_to_string('../templates/html-email/vacancy_selected.html', { 'user': application.user })
+
+        send_mail(
+            subject,
+            '',
+            from_email,
+            to_email,
+            fail_silently=False,
+            html_message=html_content,
+        )
+
+        if job.max_candidates == job.applications.filter(is_selected=True).count():
+            job.isClosed = True  
+            job.save()
+
+        return Response(
+            {"message": "Candidato selecionado com sucesso e outros candidatos removidos."},
+            status=status.HTTP_200_OK
+        )
--- backend/core/views/user.py
+++ backend/core/views/user.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from backend.core.models import User
+from backend.core.models import User, Job, JobApplication
 from backend.core.serializers import UserProfileSerializer
-from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,9 +10,41 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by("id")
     serializer_class = UserProfileSerializer
 
-    @action (detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
     def me(self, request):
         """Retorna o usuário autenticado"""
         user = request.user
         serializer = UserProfileSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def accept_vacancy(self, request, pk=None):
+        """Permite que o usuário se candidate a uma vaga especificada pelo job_id"""
+        job = get_object_or_404(Job, id=pk)
+        print(job)
+
+        if job.isClosed:
+            return Response(
+                {"error": "Essa vaga está fechada para novas candidaturas"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        job.check_max_candidates()
+        if job.isClosed:
+            return Response(
+                {"error": "Essa vaga está fechada para novas candidaturas"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if JobApplication.objects.filter(user=request.user, job=job).exists():
+            return Response(
+                {"error": "Você já se candidatou para essa vaga"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        JobApplication.objects.create(user=request.user, job=job, status=JobApplication.StatusJob.PENDENTE)
+
+        return Response(
+            {"message": "Candidatura realizada com sucesso"},
+            status=status.HTTP_201_CREATED
+        )
