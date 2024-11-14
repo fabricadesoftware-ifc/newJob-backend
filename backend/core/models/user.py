e9de1bac38d83b5ac9aa13b3b79a072466c104f8
GabrielLima2803 <gabriellima2803@gmail.com>
2024-11-14 16:43:22 -0300

    Fix: Removendo userJob

--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _  # Para tradução
 from backend.files.models import Image
 
-
 class UserManager(BaseUserManager):
     """Manager para usuários."""
 
